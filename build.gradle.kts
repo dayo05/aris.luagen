567345ea298318c4009728c1c8d9279106b82cad
dayo <dayo05@daum.net>
2026-02-13 00:11:11 +0800

    Bump KSP version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "2.0.0"
-    id("com.google.devtools.ksp") version "2.0.0-1.0.21"
+    kotlin("jvm") version "2.3.0"
+    id("com.google.devtools.ksp") version "2.3.5"
 }
 
 group = "me.ddayo"
