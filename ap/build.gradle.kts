35985b801e0ab8862e2d76ccdc22eeaccdaf1cea
dayo <dayo05@daum.net>
2026-02-13 00:11:11 +0800

    Bump KSP version

--- ap/build.gradle.kts
+++ ap/build.gradle.kts
@@ -6,8 +6,8 @@ group = "me.ddayo"
 version = "1.0-SNAPSHOT"
 
 dependencies {
-    implementation("com.google.devtools.ksp:symbol-processing-api:2.0.0-1.0.21")
-    implementation("party.iroiro.luajava:luajit:4.0.0")
+    implementation("com.google.devtools.ksp:symbol-processing-api:2.3.5")
+    implementation("party.iroiro.luajava:luajit:4.0.2")
     testImplementation(kotlin("test"))
 }
 
